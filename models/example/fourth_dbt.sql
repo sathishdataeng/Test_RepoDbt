0ad171d5723fae013efca4d8b66bfd210ef823f7
sathish kumar <sathishsoontobedataengineer@gmail.com>
2024-07-30 01:19:20 +0000

    test

--- models/example/fourth_dbt.sql
+++ models/example/fourth_dbt.sql
@@ -6,6 +6,6 @@
 with temptb as (
 (select * from DBT_NEW_MODEL.Test.COUNTRY
 union all
-(select * from {{ ref('my_third_dbt_model') }})
+(select * from {{ ref('erd_dbt') }})
 )
 select * from temptb
