e6dd4e2ef49203e774cc264683ec91c513117f57
sathish kumar <sathishsoontobedataengineer@gmail.com>
2024-07-30 23:27:49 +0000

    new stage

---
+++ models/stage/based_jodid_sala.sql
@@ -0,0 +1,17 @@
+--based_jodid_sala.sql
+
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+
+{% set job_ids= ["IT_PROG","AD_PRES","ST_CLERK","SA_REP"]%}
+
+select job_id,
+{%- for j_id in job_ids -%}
+sum(case when job_id='{{j_id}}' then salary end) as {{j_id}}_tot_amt
+{% if not loop.last %},{% endif %}
+{%- endfor -%}
+from {{ ref('raw_employee') }} group by 1
