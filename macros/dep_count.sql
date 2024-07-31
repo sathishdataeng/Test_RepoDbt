f0c5634a623f51902288299068f2079ea91ae8fa
sathish kumar <sathishsoontobedataengineer@gmail.com>
2024-07-31 21:11:38 +0000

    macro and stage

--- macros/dep_count.sql
+++ macros/dep_count.sql
@@ -3,4 +3,6 @@
 select {{dep_tbl}}.department_name ,count{{emp_tbl}}.employee_id) as num_employ
 from {{dep_tbl}} inner join {{emp_tbl}} on {{dep_tbl}}.department_id={{emp_tbl}}.department_id
 group by {{dep_tbl}}.department_name
+
+
 {% endmacro %}
