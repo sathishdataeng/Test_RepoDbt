4311ef0f0c5f56f1aaa92705f80b31ae4653ad29
sathish kumar <sathishsoontobedataengineer@gmail.com>
2024-07-31 20:01:32 +0000

    surrogate key generated for salary hike

--- models/stage/employee_hike_per_emp.sql
+++ models/stage/employee_hike_per_emp.sql
@@ -7,4 +7,6 @@
 }}
 
 
-select employee_id,first_name,{{sal_hike(25)}} as After_sal_hike from {{ ref('raw_employee') }}
+select {{ dbt_utils.generate_surrogate_key(['EMPLOYEE_ID', 'FIRST_NAME','JOB_ID']) }} ,employee_id,first_name,{{sal_hike(25)}} as After_sal_hike from {{ ref('raw_employee') }}
+
+select * from {{ ref('raw_employee') }}
