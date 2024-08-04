c6167d7744a3f6e74aa56eb7d1d9cedf071f993d
sathish kumar <sathishsoontobedataengineer@gmail.com>
2024-08-04 07:58:16 +0000

    relationship added

--- models/raw/raw_employee.sql
+++ models/raw/raw_employee.sql
@@ -5,3 +5,6 @@
 }}
 
 select * from  {{ source('Test_dbt', 'employee') }}
+
+
+select job_id,count(job_id) from data_dbt.data_schema.raw_employee group by job_id having count(*)>1
--- tests/emp_duplicatecheck_test.sql
+++ tests/emp_duplicatecheck_test.sql
@@ -1,1 +1,6 @@
-select * from job_id,count(job_id) from {{ ref('raw_employee') }} group by job_id where count(*)>1
+with tmp as(
+    select * from {{ ref('raw_employee') }}
+)
+
+select job_id,count(job_id) from {{ ref('raw_employee') }} group by job_id having count(*)>1
+
