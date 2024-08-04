b5766078cc81bdc36c9f120b8420d4938ee1427e
sathish kumar <sathishsoontobedataengineer@gmail.com>
2024-08-04 08:04:31 +0000

    relationship created

--- models/raw/raw_employee.sql
+++ models/raw/raw_employee.sql
@@ -8,3 +8,24 @@ select * from  {{ source('Test_dbt', 'employee') }}
 
 
 select job_id,count(job_id) from data_dbt.data_schema.raw_employee group by job_id having count(*)>1
+
+
+with child as (
+    select DEPARTMENT_ID as from_field
+    from data_dbt.data_schema.raw_employee
+    where DEPARTMENT_ID is not null
+),
+
+parent as (
+    select DEPARTMENT_ID as to_field
+    from data_dbt.data_schema.raw_department
+)
+
+select
+    from_field
+
+from child
+left join parent
+    on child.from_field = parent.to_field
+
+where parent.to_field is null
